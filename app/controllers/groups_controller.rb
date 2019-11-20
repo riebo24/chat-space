3c520107181fca8f61926703e1b138446bda380c
rie1224 <a10ea211@yahoo.co.jp>
2019-11-21 00:26:12 +0900

    set group action
    グループテーブルに
    new create edit updateアクションを追加。

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,18 @@
 class GroupsController < ApplicationController
-   
+   def new
+   end
+
+   def create
+    Group.create(id: params[:id],group_name: params[group_name])
+    redirect_to controller: :chats, action: :index       
+   end
+
+   def edit       
+   end
+
+   def updat
+    Group.update(id: params[:id],group_name: params[group_name])
+    redirect_to controller: :chats, action: :index        
+   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "messages#index"
+  root to: "chats#index"
   resources :users, only: [:edit, :update]
   resources :users, only: [:new, :create, :edit, :update]
 end
