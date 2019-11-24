65eff2333cb67120d7301a210ff6f9dddf8c3b69
rie1224 <a10ea211@yahoo.co.jp>
2019-11-24 12:19:06 +0900

    set_chats
    Chatモデルの作成。
    （アソシエーションの設定。）
    
    chatsテーブルの作成。
    ルーティングも設定。

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,5 @@
 class Group < ApplicationRecord
-#   has_many chats
+  has_many :chats
   has_many :group_users
   has_many :users, through: :group_users
   validates :group_name, presence: true, uniqueness: true
