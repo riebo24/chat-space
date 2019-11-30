9fdb6dfdf9c3b7fa7dfe6266fcce52da2d7ecadd
rie1224 <a10ea211@yahoo.co.jp>
2019-11-30 20:24:21 +0900

    setting_message
    メッセージ機能の実装

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,4 +3,12 @@ class Group < ApplicationRecord
   has_many :group_users
   has_many :users, through: :group_users
   validates :group_name, presence: true, uniqueness: true
+  
+  def show_last_chat
+    if (last_chat = chats.last).present?
+      last_chat.text? ? last_chat.text : "画像が投稿されています"
+    else
+      "まだメッセージはありません"
+    end
+  end
 end
